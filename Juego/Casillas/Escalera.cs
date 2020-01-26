e939b0ccd853a6babf02775fb1898bb81ff8f1e7
Julio Diaz <diaz.cesar.julio@gmail.com>
2020-01-26 04:56:43 -0400

    TIpos de casillas

---
+++ Juego/Casillas/Escalera.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Juego.Casillas {
+    class Escalera : Obstaculo{
+
+        public Escalera(int Origen, int Destino) {
+            this.Origen = Origen;
+            this.Destino = Destino;
+        }
+
+        public override bool VerificarCasilla(Juego juego, Jugador jugador) {
+            bool isInOrigin = false;
+            if (jugador.Posicion == Origen) {
+                jugador.Ascender(Destino);
+                isInOrigin = true;
+            }
+            return isInOrigin;
+        }
+    }
+}
---
+++ Juego/Casillas/Obstaculo.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Juego.Casillas {
+    abstract class Obstaculo {
+
+        protected int Origen { get; set; }
+        protected int Destino { get; set; }
+
+        protected virtual int GetDestino() {
+            return Destino;
+        }
+
+        protected virtual void SetDestino(int value) {
+            Destino = value;
+        }
+
+        public abstract bool VerificarCasilla(Juego juego ,Jugador jugador);
+
+
+    }
+}
